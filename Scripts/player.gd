eeb0d2fb39b70b631eae44fea1388816e3d50ce0
MichaelG-DTE <21051@rosmini.school.nz>
2025-03-10 14:20:54 +1300

    Added Background, player thrusters, and script for level one

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -7,14 +7,18 @@ func _process(delta):
 	var direction = Input.get_vector("Left", "Right", "Up", "Down")
 	if direction.x > 0:
 		$Shiptexture.animation = "Ship_Right"
-		$Thrusters2.animation = "Thrusters_Right"
-	
+		$Shiptexture/BankingThrusters.animation= "Thrusters_Right"
+		$Shiptexture/BankingThrusters.visible = true 
+		$Shiptexture/Thrusters.visible = false
 	elif direction.x < 0:
 		$Shiptexture.animation = "Ship_Left"
-		$Thrusters2.animation = "Thrusters_Left"
-		
+		$Shiptexture/BankingThrusters.animation = "Thrusters_Left"
+		$Shiptexture/BankingThrusters.visible = true 
+		$Shiptexture/Thrusters.visible = false
 	else: 
 		$Shiptexture.animation = "Ship"
-		$Thrusters.animation = "Thrusters"
+		$Shiptexture/Thrusters.animation = "Thrusters"
+		$Shiptexture/BankingThrusters.visible = false
+		$Shiptexture/Thrusters.visible = true
 	velocity = direction * speed
 	move_and_slide()
