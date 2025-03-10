ffe8d5aa7c0ac3164401a8965059f7f0ffcb8a3b
MichaelG-DTE <21051@rosmini.school.nz>
2025-03-10 21:19:01 +1300

    THRUSTERS ARE WORKING! IT took a little bit, and some thinking, but I DID IT! Left and Right thrusters play animations when left and right arrow keys are pressed.

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -7,18 +7,20 @@ func _process(delta):
 	var direction = Input.get_vector("Left", "Right", "Up", "Down")
 	if direction.x > 0:
 		$Shiptexture.animation = "Ship_Right"
-		$Shiptexture/BankingThrusters.animation= "Thrusters_Right"
-		$Shiptexture/BankingThrusters.visible = true 
+		$Shiptexture/ThrustersAnimation.play("RightThrusters")
+		$Shiptexture/Thrusters_Right.visible = true
 		$Shiptexture/Thrusters.visible = false
+		$Shiptexture/Thrusters_Left.visible = false 
 	elif direction.x < 0:
 		$Shiptexture.animation = "Ship_Left"
-		$Shiptexture/BankingThrusters.animation = "Thrusters_Left"
-		$Shiptexture/BankingThrusters.visible = true 
+		$Shiptexture/ThrustersAnimation.play("LeftThrusters")
+		$Shiptexture/Thrusters_Left.visible = true
 		$Shiptexture/Thrusters.visible = false
+		$Shiptexture/Thrusters_Right.visible = false
 	else: 
 		$Shiptexture.animation = "Ship"
-		$Shiptexture/Thrusters.animation = "Thrusters"
-		$Shiptexture/BankingThrusters.visible = false
 		$Shiptexture/Thrusters.visible = true
+		$Shiptexture/Thrusters_Left.visible = false 
+		$Shiptexture/Thrusters_Right.visible = false
 	velocity = direction * speed
 	move_and_slide()
